ee1f817aa7f073564442bcfd0e016b50d3007844
ArtSoller <art-sav2002@yandex.ru>
2023-11-23 23:36:56 +0700

    UNREAL 5 commit

---
+++ Packman.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+
+namespace WpfApp1
+{
+    public class Packman
+    {
+        public Rectangle Position { get; set; }
+        public Packman(Canvas Canvas) 
+        {
+            Position = new Rectangle();
+            Position.Name = "pacman";
+            Position.Width = 30;
+            Position.Height = 30;
+            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+            mySolidColorBrush.Color = Color.FromArgb(255, 255, 255, 0);
+            Position.Fill = mySolidColorBrush;
+        }
+        //<Rectangle Name = "pacman" Width="30" Height="30" Fill="Yellow" Canvas.Left="100" Canvas.Top="260" />
+
+    }
+}
